e37cbe19bb644f05e5d2a6772a07fcbd34cadc44
11cookies11 <gao13048663696@163.com>
2025-11-18 23:53:45 +0800

    feat(stm32): 配置stm32f103芯片，并且可以正常仿真。

--- src/main.c
+++ src/main.c
@@ -1,4 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t tmp_var = 0;
+
 int main(void)
 {
-    while (1);
+    while(1)
+    {
+        tmp_var++;
+        for (size_t i = 0; i < 10000; i++)
+        {
+            /* code */
+            asm volatile("nop");
+        }
+    }
 }
